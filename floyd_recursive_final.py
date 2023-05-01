6f88b9b6fa698d710cd7745b8520c3dfe1c974de
cavara1976 <129801146+cavara1976@users.noreply.github.com>
2023-05-01 13:59:39 +0100

    Add files via upload

--- floyd_recursive_final.py
+++ floyd_recursive_final.py
@@ -6,34 +6,35 @@ import timeit
 
 
 NO_PATH = sys.maxsize
+
 global graph
+
 graph = [[0, 7, NO_PATH, 8],
          [NO_PATH, 0, 5, NO_PATH],
          [NO_PATH, NO_PATH, 0, 2],
          [NO_PATH, NO_PATH, NO_PATH, 0]]
+
 MAX_LENGTH: int = len(graph[0])
 
 
 
-# New function calculating the shortest path using a recursive function
+# New function that calculates the shortest path using a recursive function
 def shortestPath(i, j, k, d):
-        #distance = list(map(lambda i: list(map(lambda j: j, i)), graph))
+       
         # Return weight when there are no intermediary vertices
-
         # base condition - no intermediate vertices are used
-        if k == -1 or k <0:
-         
+        if k == 0:
+            
             return (d[i][j])
 
-        # If travelling via k is shorter return shorter distance
+        # recursive condition  - If distance beterrn the first node and the intermediate node k is shorter that i and j return shorter distance
         else:
            
-            #print (distance[i][j])
             return min(shortestPath(i, j, k-1, d),
                        shortestPath(i, k, k - 1,d) + shortestPath(k, j, k - 1,d))
          
 
-
+# New function to scan the graphs nodes and call the shortestPath function to create a matrix of the shortest paths
 
 def printSolution(distance):
     print("Following matrix shows the shortest distances between every pair of vertices: ")
@@ -47,7 +48,7 @@ def printSolution(distance):
             distance[start_node][end_node] = shortestPath(start_node,
                                                       end_node,
                                                       MAX_LENGTH-1, distance)
-            print(distance)   
+            #print(distance)   
     return distance
 
 
