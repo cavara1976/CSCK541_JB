2ed60555fe88345efcb5ba9a95c1509179aad4ea
cavara1976 <129801146+cavara1976@users.noreply.github.com>
2023-05-01 13:20:26 +0100

    Final code segments for assignment

---
+++ floyd_imperative_final.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import sys
+import itertools
+import timeit
+
+NO_PATH = sys.maxsize
+
+global graph
+graph = [[0, 7, NO_PATH, 8],
+         [NO_PATH, 0, 5, NO_PATH],
+         [NO_PATH, NO_PATH, 0, 2],
+         [NO_PATH, NO_PATH, NO_PATH, 0]]
+MAX_LENGTH: int = len(graph[0])
+
+#This function implements the imperative version of the Floyd Marshal algorithm using iteration
+
+def floyd(distance):
+    """
+    A simple implementation of Floyd's algorithm
+    """
+    for intermediate, start_node, end_node in itertools.product(range(MAX_LENGTH), range(MAX_LENGTH),
+                         range(MAX_LENGTH)):
+
+        # Assume that distance is zero if tart_node == end_node 
+      
+        if start_node == end_node:
+            distance[start_node][end_node] = 0
+            continue
+
+        # rfind the minimum duistance and return the path result
+        distance[start_node][end_node] = min(distance[start_node][end_node],distance[start_node][intermediate] +
+                                             distance[intermediate][end_node])
+
+    # Any value that have sys.maxsize has no path
+    print(distance)
+
+
+# Driver's code
+if __name__ == '__main__':
+
+# Calls the function printSolution and passes the graph as an argument 
+    floyd(graph)
+
+print("<<><><>><><><><><><><>")
+print("Timeit Performance Test Result: ")
+print (timeit.timeit())
+print("<<><><>><><><><><><><>")
+
+
---
+++ floyd_recursive_final.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import sys
+import itertools
+import timeit
+
+
+NO_PATH = sys.maxsize
+
+global graph
+
+graph = [[0, 7, NO_PATH, 8],
+         [NO_PATH, 0, 5, NO_PATH],
+         [NO_PATH, NO_PATH, 0, 2],
+         [NO_PATH, NO_PATH, NO_PATH, 0]]
+
+MAX_LENGTH: int = len(graph[0])
+
+
+
+# New function that calculates the shortest path using a recursive function
+def shortestPath(i, j, k, d):
+       
+        # Return weight when there are no intermediary vertices
+        # base condition - no intermediate vertices are used
+        if k == 0:
+            
+            return (d[i][j])
+
+        # recursive condition  - If distance beterrn the first node and the intermediate node k is shorter that i and j return shorter distance
+        else:
+           
+            return min(shortestPath(i, j, k-1, d),
+                       shortestPath(i, k, k - 1,d) + shortestPath(k, j, k - 1,d))
+         
+
+# New function to scan the graphs nodes and call the shortestPath function to create a matrix of the shortest paths
+
+def printSolution(distance):
+    print("Following matrix shows the shortest distances between every pair of vertices: ")
+    
+    for start_node, end_node in itertools.product(range(MAX_LENGTH),
+                                                  range(MAX_LENGTH)):
+            if start_node == end_node:
+                distance[start_node][end_node] = 0
+                continue
+         
+            distance[start_node][end_node] = shortestPath(start_node,
+                                                      end_node,
+                                                      MAX_LENGTH-1, distance)
+            #print(distance)   
+    return distance
+
+
+
+# Driver's code
+if __name__ == '__main__':
+
+# Calls the function printSolution and passes the graph as an argument 
+    print(printSolution(graph))
+
+print("<<><><>><><><><><><><>")
+print("Timeit Performance Test Result: ")
+print (timeit.timeit())
+print("<<><><>><><><><><><><>")
+
+  
